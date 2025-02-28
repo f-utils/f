58e5e23a1270289518f25cca9596ee75d0dd2333
yxm <yxm.dev@gmx.ie>
2025-02-28 11:44:30 -0300

    feat: add error classes

---
+++ f/mods/err_.py
@@ -0,0 +1,20 @@
+class ConfErr(Exception):
+    pass
+
+class MetaErr(Exception):
+    pass
+
+class OpErr(Exception):
+    pass
+
+class SpecErr(Exception):
+    pass
+
+class DSpecErr(Exception):
+    pass
+
+class TypeErr(Exception):
+    pass
+
+class HelperErr(Exception):
+    pass
