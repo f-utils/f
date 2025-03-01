5f3071a8784106766519eb0ac462073a14da458c
yxm <yxm.dev@gmx.ie>
2025-03-01 18:56:07 -0300

    ref: replace the use of metaclasses in spec_.py with a spec_ function

--- f/mods/spec_.py
+++ f/mods/spec_.py
@@ -1,93 +1,150 @@
 from f.mods.meta_ import Meta
 from f.mods.err_  import SpecErr
+from f.mods.update_ import Update
 
-class _spec(type):
-    def __new__(mcs, name, bases, dct, **kwargs):
-        cls = super().__new__(mcs, name, bases, dct)
-        cls.at = kwargs.get('at', None)
-        cls.att = kwargs.get('att', None)
-        return cls
+def spec_(at, att):
+    class Spec(Meta):
+        def __new__(cls, spec_name, base_at=None):
+            specs_dict = base_at if base_at is not None else at
+            if spec_name not in specs_dict:
+                raise SpecErr(f"Spectrum '{spec_name}' not found in database '{base_at}'.")
+            def exec_func(*args, **kwargs):
+                spec = specs_dict[spec_name]
+                for arg_types, funcinfo in spec['spec']['body'].items():
+                    acceptable_types = f.acceptable_types_()
+                    if not len(args) == len(arg_types):
+                        raise SpecErr(f"Expected '{len(arg_types)}' arguments. Received: '{len(args)}'.")
+                    if all(isinstance(arg, typ) for arg, typ in zip(args, arg_types) if typ in acceptable_types):
+                        return funcinfo['func'](*args, **kwargs)
+                mismatch_types = [type(arg).__name__ for arg in args if type(arg) not in arg_types]
+                if mismatch_types:
+                    raise SpecErr(f"Types '{mismatch_types}' are not in the domain of spectrum '{spec_name}'.")
+            return exec_func
 
-    def database(cls, *args):
-        return Meta.database(cls.at, *args)
-    db = database
+        @staticmethod
+        def database(*args):
+            return Meta.database(at, *args)
+        db = database
 
-    def init(cls, spec_name, description, std):
-        return Meta.init(spec_name, description, std, cls.at)
-    i = init
+        @staticmethod
+        def init(spec_name, description, std):
+            return Meta.init(spec_name, description, std, at)
+        i = init
 
-    def extend(cls, spec_name, arg_types, func):
-        from f.main import f
-        from itertools import product
-        expanded_arg_types = []
-        for typ in arg_types:
-            if isinstance(typ, list):
-                if not all(isinstance(t, type) for t in typ):
-                    raise TypeError("All elements in the list must be types.")
-                expanded_arg_types.append(tuple(typ))
-            elif typ in ('any', 'Any'):
-                expanded_arg_types.append(f.acceptable_types_())
-            else:
-                if not isinstance(typ, type):
-                    raise TypeError(f"'{typ}' is not a valid type.")
-                expanded_arg_types.append((typ,))
-        type_combinations = product(*expanded_arg_types)
-        for combo in type_combinations:
-            Meta.extend(spec_name, combo, func, cls.at, cls.att)
-    e = extend
+        @staticmethod
+        def extend(spec_name, arg_types, func):
+            from f.main import f
+            from itertools import product
+            expanded_arg_types = []
+            for typ in arg_types:
+                if isinstance(typ, list):
+                    if not all(isinstance(t, type) for t in typ):
+                        raise TypeError("All elements in the list must be types.")
+                    expanded_arg_types.append(tuple(typ))
+                elif typ in ('any', 'Any'):
+                    expanded_arg_types.append(f.acceptable_types_())
+                else:
+                    if not isinstance(typ, type):
+                        raise TypeError(f"'{typ}' is not a valid type.")
+                    expanded_arg_types.append((typ,))
+            type_combinations = product(*expanded_arg_types)
+            for combo in type_combinations:
+                Meta.extend(spec_name, combo, func, at, att)
+        e = extend
 
-    def add(cls, spec_name, attribute):
-        aliases = {
-            'comments': ['c', 'comment'],
-            'tags': ['t', 'tag']
-        }
-        return Meta.add(spec_name, attribute, cls.at, aliases)
-    a = add
+        @staticmethod
+        def add(spec_name, attribute):
+            aliases = {'comments': ['c', 'comment'], 'tags': ['t', 'tag']}
+            return Meta.add(spec_name, attribute, at, aliases)
+        a = add
 
-    def delete(cls, spec_name, attribute):
-        aliases = {
-            'comments': ['c', 'comment'],
-            'tags': ['t', 'tag']
-        }
-        return Meta.delete(spec_name, attribute, cls.at, aliases)
-    d = delete
+        @staticmethod
+        def delete(spec_name, attribute):
+            aliases = {'comments': ['c', 'comment'], 'tags': ['t', 'tag']}
+            return Meta.delete(spec_name, attribute, at, aliases)
+        d = delete
 
-    def update(cls, spec_name, attribute):
-        aliases = {
-            'description': ['d', 'desc', 'description'],
-            'tags': ['t', 'tag', 'tags'],
-            'comments': ['c', 'comment', 'comments'],
-            'std': ['s', 'std', 'standard'],
-            'body': ['b', 'body']
-        }
-        return Meta.update(spec_name, attribute, cls.at, aliases)
-    u = update
+        @staticmethod
+        def export():
+            return Meta.export(at)
+        E = export
 
-    def export(cls):
-        return Meta.export(cls.at)
-    E = export
+        @staticmethod
+        def check(spec_names):
+            return Meta.check(spec_names, at)
 
-    def check(cls, spec_names):
-        return Meta.check(spec_names, cls.at)
-    c = check
+        @staticmethod
+        def get(spec_name, attribute):
+            aliases = {
+                'description': ['d', 'desc', 'description'],
+                'tags': ['t', 'tag', 'tags'],
+                'comments': ['c', 'comment', 'comments'],
+                'std': ['s', 'std', 'standard'],
+                'body': ['b', 'body']
+            }
+            return Meta.get(spec_name, attribute, at, aliases)
+        g = get
 
-    def search(cls, term, where):
-        aliases = {
-            'description': ['d', 'desc'],
-            'tags': ['t', 'tag'],
-            'comments': ['c', 'comment'],
-        }
-        return Meta.search(term, where, cls.at, aliases)
-    s = search
+        @staticmethod
+        def search(term, where):
+            aliases = {
+                'description': ['d', 'desc'],
+                'tags': ['t', 'tag'],
+                'comments': ['c', 'comment'],
+            }
+            return Meta.search(term, where, at, aliases)
+        s = search
 
-    def info(cls, spec_name):
-        aliases = {
-            'description': ['d', 'desc', 'description'],
-            'tags': ['t', 'tag', 'tags'],
-            'comments': ['c', 'comment', 'comments'],
-            'std': ['s', 'std', 'standard'],
-            'body': ['b', 'body'],
-            'domain': ['dm', 'domain']
-        }
-        return Meta.info(spec_name, cls.at, aliases)
-    I = info
+        @staticmethod
+        def info(spec_name):
+            aliases = {
+                'description': ['d', 'desc', 'description'],
+                'tags': ['t', 'tag', 'tags'],
+                'comments': ['c', 'comment', 'comments'],
+                'std': ['s', 'std', 'standard'],
+                'body': ['b', 'body'],
+                'domain': ['dm', 'domain']
+            }
+            return Meta.info(spec_name, at, aliases)
+        I = info
+
+        class update:
+            aliases_ = {
+                'std': ['s', 'std', 'standard'],
+                'body': ['b', 'body']
+            }
+            updaters_ = {
+                'std': lambda spec_info, new_std: Update.std(spec_info, new_std),
+                'body': lambda spec_info, domain, new_func: Update.body(spec_info, domain, new_func)
+            }
+
+            @staticmethod
+            def desc(spec_name, new_description):
+                return Meta.update(spec_name, 'description', at)(new_description)
+
+            @staticmethod
+            def tag(spec_name, old_tag, new_tag):
+                return Meta.update(spec_name, 'tags', at)(old_tag, new_tag)
+
+            @staticmethod
+            def comment(spec_name, comment_id, new_comment_value):
+                return Meta.update(spec_name, 'comments', at)(comment_id, new_comment_value)
+
+            @staticmethod
+            def std(spec_name, new_std):
+                def updater(spec_info):
+                    return Spec.update.updaters_['std'](spec_info, new_std)
+
+                # Retrieve the specific entry from `at`
+                if spec_name in at:
+                    return updater(at[spec_name])
+                else:
+                    raise SpecErr(f"Spectrum '{spec_name}' not found in database '{at}'.") 
+
+            @staticmethod
+            def body(spec_name, domain, new_func):
+                resolved_body = Spec.update.aliases_['body'][0]
+                return Meta.update(spec_name, resolved_body, at, Spec.update.updaters_, Spec.aliases_)(domain, new_func)
+        u = update
+
+    return Spec
